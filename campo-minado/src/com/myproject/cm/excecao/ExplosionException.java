5f458bdde60e90f5038034daffa99674f9e8d2be
Gabriel W. D. Ferraz <84943221+Gabriel-wil@users.noreply.github.com>
2021-10-15 14:29:21 -0300

    Delete campo-minado directory

--- campo-minado/src/com/myproject/cm/excecao/ExplosionException.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.cm.excecao;
-
-@SuppressWarnings("serial")
-public class ExplosionException extends RuntimeException {
-
-	
-}
--- campo-minado/src/com/myproject/cm/excecao/SairExecption.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.cm.excecao;
-
-@SuppressWarnings("serial")
-public class SairExecption extends RuntimeException {
-
-	
-}
